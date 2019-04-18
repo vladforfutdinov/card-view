403c2c1245796bd5187be5f5245938dd677721fe
Volodymyr Forfutdinov <volodymyr_forfutdinov@epam.com>
2019-04-18 23:30:27 +0300

    - clean manifest.js; - add link copy ability;

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 (function () {
     const defaultNumber = '0000000000000000';
+    const hash = location.hash.replace('#', '');
+
     const form = document.forms[0];
     const numberPlace = document.getElementById('number');
     const numberInput = document.getElementById('number-input');
-    const hash = location.hash.replace('#', '');
+    const copyButton = document.getElementById('copy-button');
 
     if (hash) {
         numberInput.value = hash;
     }
 
+    setNumber();
+
     window.addEventListener('hashchange', setNumber);
     form.addEventListener('submit', setNumber);
     numberInput.addEventListener('keyup', setNumber);
-
-    setNumber();
+    copyButton.addEventListener('click', copyLocation);
 
     function setNumber(e) {
         if (e) {
@@ -24,15 +27,42 @@
             }
         }
 
-        const value = numberInput.value || defaultNumber;
-        const normalized = value.replace(/\s/g, '').trim();
-        const matches = normalized.match(/\d{4}/g);
+        const value = getNormalized();
+        const isValid = isNumberValid(value);
 
-        if (matches && matches.length === 4 && valid_credit_card(normalized)) {
-            numberPlace.innerHTML = matches.join(' ');
-        } else {
-            numberPlace.innerHTML = 'invalid number'.toUpperCase();
-        }
+        history.pushState(null, null, isValid ? `#${value}` : ' ');
+        numberPlace.innerHTML = isValid ? getFormattedNumber(value) : 'invalid number'.toUpperCase();
+        copyButton.disabled = !isValid;
+    }
+
+    function isNumberValid(normalized) {
+        const matches = normalized.match(/\d{4}/g);
+        return matches && matches.length === 4 && valid_credit_card(normalized);
+    }
+
+    function getNormalized() {
+        const value = numberInput.value || defaultNumber;
+        return value.replace(/\s/g, '').trim();
+    }
+
+    function getFormattedNumber(normalized) {
+        return normalized.match(/\d{4}/g).join(' ');
+    }
+
+    function copyLocation(e) {
+        e.preventDefault();
+        navigator.clipboard.writeText(location.href)
+            .then(() => {
+                copyButton.innerText = 'copied';
+                copyButton.disabled = true;
+
+                setTimeout(resetButton, 3000);
+            });
+    }
+
+    function resetButton() {
+        copyButton.innerText = 'Copy link';
+        copyButton.disabled = !isNumberValid(getNormalized());
     }
 
     function valid_credit_card(value) {
