bddda7b1e8ab45636f6738c33063dfc4249dbe7d
Volodymyr Forfutdinov <volodymyr_forfutdinov@epam.com>
2019-04-19 00:02:32 +0300

    - remove redundant console.log;

--- sw.js
+++ sw.js
@@ -23,7 +23,6 @@ importScripts('./cache-polyfill.js');
     });
 
     self.addEventListener('fetch', function (event) {
-        console.log(event.request.url);
         event.respondWith(
             caches.match(event.request).then(function (response) {
                 return response || fetch(event.request);
